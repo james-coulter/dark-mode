e581e5b6ba443f2bd45a4a1276382a3f21a4f2a1
Christian Coulter <95coulter@gmail.com>
2020-05-06 11:43:42 -0600

    Finished Stretch by styling charts with different syles.

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -9,6 +9,7 @@ import {
   Tooltip,
 } from "recharts";
 
+
 const Chart = ({ sparklineData }) => {
   const formattedData = sparklineData
     .map((price, idx) => {
@@ -29,8 +30,8 @@ const Chart = ({ sparklineData }) => {
   return (
 <>
     <LineChart width={1100} height={300} data={formattedData}>
-      <Line type="monotone" dataKey="value" stroke="#8884d8" />
-      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <Line type="monotone" dataKey="value" stroke="#FFA500" className='lineValue'/>
+      <CartesianGrid stroke="#191414" strokeDasharray="5 5" />
       <XAxis dataKey="date" interval={3} />
       <YAxis />
       <Tooltip />
